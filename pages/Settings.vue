93e6c7e3958dce7c9893386e8a18174fd8623953
Maksimka2280 <maksimkalol1402@gmail.com>
2024-09-21 23:37:15 +0300

    add validation in Settings

--- pages/Settings.vue
+++ pages/Settings.vue
@@ -32,67 +32,75 @@
                                 phone number
                             </button>
                         </div>
-                        <form action="">
+                        <form @submit.prevent="validateForm">
                             <fieldset>
                                 <div class="flex flex-col md:flex-row gap-[20px]">
-                                    <div class="">
-                                        <label for="emirate"
+                                    <div>
+                                        <label for="fullName"
                                             class="font-roboto text-[16px] font-normal leading-[19.2px] text-[#000000]">
                                             Full name:
                                         </label>
-                                        <input type="name"
-                                            class="w-[330px]  max-w-[426px]  lg:w-[430px] sm:w-[430px] h-[50px] mt-[5px] rounded-full border block border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]">
-
+                                        <input id="fullName" type="text" v-model="form.name" @input="errors.name = ''"
+                                            :class="{ 'border-red-500': errors.name }"
+                                            class="w-[330px] max-w-[426px] lg:w-[430px] sm:w-[430px] h-[50px] mt-[5px] rounded-full border block border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]" />
+                                        <span v-if="errors.name" class="text-red-500">{{ errors.name }}</span>
                                     </div>
-                                    <div class="w-full">
-                                        <label for="code"
-                                            class="font-roboto text-[16px] font-normal leading-[19.2px] text-[#000000]">Mobile
-                                            number:</label>
-                                        <input type="number"
-                                            class="w-[330px]  max-w-[426px]  lg:w-[430px] sm:w-[430px] h-[50px] mt-[5px] rounded-full border block border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]">
 
+                                    <div class="w-full">
+                                        <label for="mobile"
+                                            class="font-roboto text-[16px] font-normal leading-[19.2px] text-[#000000]">
+                                            Mobile number:
+                                        </label>
+                                        <input id="mobile" type="number" v-model="form.mobile"
+                                            @input="errors.mobile = ''" :class="{ 'border-red-500': errors.mobile }"
+                                            class="w-[330px] max-w-[426px] lg:w-[430px] sm:w-[430px] h-[50px] mt-[5px] rounded-full border block border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]" />
+                                        <span v-if="errors.mobile" class="text-red-500">{{ errors.mobile }}</span>
                                     </div>
                                 </div>
+
                                 <div class="flex flex-col md:flex-row gap-[20px] mt-[30px] mb-[30px]">
-                                    <div class="">
-                                        <label for="sort"
+                                    <div>
+                                        <label for="design"
                                             class="font-roboto text-[16px] font-normal leading-[19.2px] text-[#000000]">Design:</label>
-                                        <input type="date"
-                                            class="w-[330px]  max-w-[426px]  lg:w-[430px] sm:w-[430px] h-[50px] mt-[5px] rounded-full border block border-[#BFBFBF] pl-4 text-lg text-[#BFBFBF] bg-[#FAFAFA]">
+                                        <input id="design" type="date" v-model="form.design"
+                                            class="w-[330px] max-w-[426px] lg:w-[430px] sm:w-[430px] h-[50px] mt-[5px] rounded-full border block border-[#BFBFBF] pl-4 text-lg text-[#BFBFBF] bg-[#FAFAFA]" />
                                     </div>
                                     <div class="w-full">
                                         <label for="emirate"
                                             class="font-roboto text-[16px] font-normal leading-[19.2px] text-[#000000]">
                                             Emirate:
                                         </label>
-                                        <select id="emirate" name="emirate"
-                                            class="border-[#BFBFBF]   w-[330px]  max-w-[426px]  lg:w-[430px] mt-[5px] text-[16px] font-normal leading-[19.2px] text-left block md:w-[430px] bg-[#FAFAFA] border rounded-[25px] py-[15px] px-[20px]">
-                                            <option value="city">Dubai</option>
-                                            <option value="city">Abu Dhabi</option>
-                                            <option value="city">Ajman</option>
-                                            <option value="city">Umm Al Quwain</option>
-                                            <option value="city">Ras Al Khaimah</option>
-                                            <option value="city">None</option>
+                                        <select id="emirate" v-model="form.emirate"
+                                            class="border-[#BFBFBF] w-[330px] max-w-[426px] lg:w-[430px] mt-[5px] text-[16px] font-normal leading-[19.2px] text-left block md:w-[430px] bg-[#FAFAFA] border rounded-[25px] py-[15px] px-[20px]">
+                                            <option value="Dubai">Dubai</option>
+                                            <option value="Abu Dhabi">Abu Dhabi</option>
+                                            <option value="Ajman">Ajman</option>
+                                            <option value="Umm Al Quwain">Umm Al Quwain</option>
+                                            <option value="Ras Al Khaimah">Ras Al Khaimah</option>
+                                            <option value="None">None</option>
                                         </select>
-
                                     </div>
-
                                 </div>
+
                                 <div class="flex flex-col sm:flex-row gap-4 mt-[70px]">
-                                    <div class="flex flex-col w-full sm:w-[430px]">
+                                    <div class="flex flex-col w-full sm:w-[430px] relative">
                                         <label for="email"
                                             class="text-[16px] font-normal leading-[19.2px] text-[#000000] mb-2">Your
                                             Email:</label>
-                                        <input id="email" type="email"
-                                            class="h-[50px] w-[330px]  max-w-[426px]  lg:w-[430px] rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]">
+                                        <input id="email" type="email" v-model="form.email" @input="errors.email = ''"
+                                            :class="{ 'border-red-500': errors.email }"
+                                            class="h-[50px] w-full rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]" />
+                                        <span v-if="errors.email" class="text-red-500">{{ errors.email }}</span>
                                     </div>
 
-                                    <div class="flex flex-col w-full sm:w-[430px]">
+                                    <div class="flex flex-col w-full sm:w-[430px] relative">
                                         <label for="login"
                                             class="text-[16px] font-normal leading-[19.2px] text-[#000000] mb-2">Choose
                                             Login:</label>
-                                        <input id="login" type="text"
-                                            class="h-[50px] w-[330px]  max-w-[426px]  lg:w-[430px] rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]">
+                                        <input id="login" type="text" v-model="form.login" @input="errors.login = ''"
+                                            :class="{ 'border-red-500': errors.login }"
+                                            class="h-[50px] w-full rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]" />
+                                        <span v-if="errors.login" class="text-red-500">{{ errors.login }}</span>
                                     </div>
                                 </div>
 
@@ -107,23 +115,33 @@
                                 If you notice a suspicious account login, change your password.
                             </p>
                             <div class="flex flex-col sm:flex-row gap-4 mt-[30px]">
-                                <div class="flex flex-col w-[330px]  max-w-[426px]  lg:w-[430px]">
-                                    <label for="email"
-                                        class="text-[16px] font-normal leading-[19.2px] text-[#000000] mb-2">Current
-                                        password:
+                                <div class="flex flex-col w-full sm:w-[430px] relative">
+                                    <label for="current-password"
+                                        class="text-[16px] font-normal leading-[19.2px] text-[#000000] mb-2">
+                                        Current password:
                                     </label>
-                                    <input id="email" type="password"
-                                        class="h-[50px] w-full rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]">
+                                    <input id="current-password" type="password" v-model="currentPassword"
+                                        @input="errors.currentPassword = ''"
+                                        :class="{ 'border-red-500': errors.currentPassword }"
+                                        class="h-[50px] w-full rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]" />
+                                    <span v-if="errors.currentPassword" class="text-red-500">{{ errors.currentPassword
+                                        }}</span>
                                 </div>
 
-                                <div class="flex flex-col w-full  lg:w-[430px]">
-                                    <label for="login"
-                                        class="text-[16px] font-normal leading-[19.2px] text-[#000000] mb-2">New
-                                        password:
+                                <div class="line hidden sm:block"></div> <!-- Линия между полями -->
+
+                                <div class="flex flex-col w-full sm:w-[430px] relative">
+                                    <label for="new-password"
+                                        class="text-[16px] font-normal leading-[19.2px] text-[#000000] mb-2">
+                                        New password:
                                     </label>
-                                    <input id="password" type="password"
-                                        class="h-[50px]  w-[330px]  max-w-[426px]  lg:w-[430px]  rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]">
+                                    <input id="new-password" type="password" v-model="newPassword"
+                                        @input="errors.newPassword = ''"
+                                        :class="{ 'border-red-500': errors.newPassword }"
+                                        class="h-[50px] w-full rounded-full border border-[#BFBFBF] pl-4 text-lg text-[#000000] bg-[#FAFAFA]" />
+                                    <span v-if="errors.newPassword" class="text-red-500">{{ errors.newPassword }}</span>
                                 </div>
+
                             </div>
                             <div class="mt-[70px]">
                                 <h2 class="w-full md:w-[872px] text-[35px] font-medium leading-[50px] text-left ">
@@ -131,7 +149,8 @@
                                 </h2>
                                 <p
                                     class="w-[380px] h-[40px] font-normal text-[16px text-[#000000] mt-[30px] text-[#BFBFBF]">
-                                    This setting will affect the order of listing types both in the main page and your dashboard.
+                                    This setting will affect the order of listing types both in the main page and your
+                                    dashboard.
                                 </p>
                             </div>
 
@@ -157,9 +176,11 @@
                                 </div>
                             </div>
                         </div>
-                        <ButtonBlue class="w-[330px] lg:w-[500px] mt-[70px]">
+                        <ButtonBlue class="w-[330px] lg:w-[500px] mt-[70px]" @click="validateForm">
                             Save changes
                         </ButtonBlue>
+
+
                     </div>
                 </div>
             </div>
@@ -188,6 +209,27 @@ export default {
             isCheckboxChecked3: false,
             selectedOption: null,
 
+            form: {
+                name: '',
+                mobile: '',
+                design: '',
+                email: '',
+                login: '',
+                emirate: '',
+            },
+            errors: {
+                name: '',
+                mobile: '',
+                email: '',
+                login: '',
+            },
+            currentPassword: '',
+            newPassword: '',
+            errors: {
+                currentPassword: '',
+                newPassword: ''
+            },
+
         };
     },
     methods: {
@@ -200,6 +242,92 @@ export default {
                 'border-[#BFBFBF] text-[#BFBFBF]': this.activeButton !== buttonType,
             };
         },
+        validateForm() {
+            this.errors = { name: '', mobile: '', email: '', login: '' }; // Clear previous errors
+
+            if (!this.form.name) {
+                this.errors.name = 'Full name is required';
+            } else if (this.form.name.length < 3) {
+                this.errors.name = 'Full name must be at least 3 characters long';
+            }
+
+            if (!this.form.mobile) {
+                this.errors.mobile = 'Mobile number is required';
+            } else if (this.form.mobile.length < 10) {
+                this.errors.mobile = 'Mobile number must be at least 10 digits';
+            }
+
+            if (!this.form.email) {
+                this.errors.email = 'Email is required';
+            } else if (!this.validateEmail(this.form.email)) {
+                this.errors.email = 'Invalid email format';
+            }
+
+            if (!this.form.login) {
+                this.errors.login = 'Login is required';
+            }
+
+            // Check if there are no errors before submitting
+            if (!Object.values(this.errors).some(error => error)) {
+                this.submitForm();
+            }
+        },
+        validateEmail(email) {
+            const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return re.test(email);
+        },
+        submitForm() {
+            // Logic for form submission
+            alert('Form submitted successfully');
+            // Here you can add your logic to save changes
+        },
+        validatePasswords() {
+            this.errors.currentPassword = '';
+            this.errors.newPassword = ''; // Clear previous errors
+
+            if (!this.form.currentPassword) {
+                this.errors.currentPassword = 'Current password is required';
+            }
+
+            if (!this.form.newPassword) {
+                this.errors.newPassword = 'New password is required';
+            } else if (this.form.newPassword.length < 8) {
+                this.errors.newPassword = 'New password must be at least 8 characters long';
+            }
+
+            // Check if there are no errors before processing
+            if (!Object.values(this.errors).some(error => error)) {
+                this.submitPasswords();
+            }
+        },
+        submitPasswords() {
+            // Logic for password submission
+            alert('Passwords submitted successfully');
+        },
+        validatePasswords() {
+            this.errors.currentPassword = '';
+            this.errors.newPassword = ''; // Сброс ошибок
+
+            if (!this.currentPassword) {
+                this.errors.currentPassword = 'Current password is required';
+            }
+
+            if (!this.newPassword) {
+                this.errors.newPassword = 'New password is required';
+            } else if (this.newPassword.length < 8) {
+                this.errors.newPassword = 'New password must be at least 8 characters long';
+            }
+
+            // Проверка, нет ли ошибок перед выполнением действия
+            if (!Object.values(this.errors).some(error => error)) {
+                this.submitPasswords();
+            }
+        },
+        submitPasswords() {
+            // Логика отправки паролей
+            alert('Passwords submitted successfully');
+        },
+
     },
     computed: {
         selectClasses() {
@@ -308,7 +436,4 @@ export default {
         font-size: 12px;
     }
 }
-
-
-
 </style>
