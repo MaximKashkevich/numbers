5f0bdf75cfe4c089d15ffc6c75d9bb91340f3098
PENGUIN <personal@konstantin-lysenko.com>
2024-09-27 03:49:29 +0200

    Правки по деплою

--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -5,6 +5,6 @@
 
 <script setup lang="ts">
 import Header from '../components/Header.vue';
-import Login from './Login.vue';
+import Login from './login.vue';
 import Footer from '../components/Footer.vue'
 </script>
