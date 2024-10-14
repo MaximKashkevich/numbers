86992b163bea9bd6ab18bf6f537b51c40dcecf15
Maksimka2280 <maksimkalol1402@gmail.com>
2024-10-14 17:48:20 +0300

    fix bug

--- stores/LikedCards.js
+++ stores/LikedCards.js
@@ -1,15 +1,15 @@
 // stores/likedCards.js
-import { defineStore } from 'pinia';
+// import { defineStore } from 'pinia';
 
 
 
-export const useLikedStore = defineStore('liked', {
-  state: () => ({
-    // likedCards: [],
-  }),
-  actions: {
-    addCard(card) {
-      this.likedCards.push(card);
-    },
-  },
-});
+// export const useLikedStore = defineStore('liked', {
+//   state: () => ({
+//     // likedCards: [],
+//   }),
+//   actions: {
+//     addCard(card) {
+//       this.likedCards.push(card);
+//     },
+//   },
+// });
