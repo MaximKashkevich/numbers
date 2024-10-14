3a645f2309fb5d0a2108d0b0948ad3e77590a426
Maksimka2280 <maksimkalol1402@gmail.com>
2024-10-14 17:51:22 +0300

    fix fucking bug

--- stores/signUp.ts
+++ stores/signUp.ts
@@ -16,3 +16,20 @@ export const useSignUpStore = defineStore("signUp", {
     },
   },
 });
+
+
+// // stores/likedCards.js
+// import { defineStore } from 'pinia';
+
+
+
+// export const useLikedStore = defineStore('liked', {
+//   state: () => ({
+//     // likedCards: [],
+//   }),
+//   actions: {
+//     addCard(card) {
+//       this.likedCards.push(card);
+//     },
+//   },
+// });
