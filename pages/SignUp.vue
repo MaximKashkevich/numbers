90859d5154cbcdd81e0394db8d94cb3eee2893dd
Maksimka2280 <maksimkalol1402@gmail.com>
2024-10-14 20:23:58 +0300

    fix bugs

--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -1,10 +1,10 @@
 <template>
 
-    <Login />
+    <login />
 </template>
 
 <script setup lang="ts">
 import Header from '../components/Header.vue';
-import Login from './Login.vue';
+import Login from './login.vue';
 import Footer from '../components/Footer.vue'
 </script>
